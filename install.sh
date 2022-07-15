cc75ce0dfb83f11da4071612f316b70d7c9f6195
Muhammad Naqdi <didi@debian-pc>
2022-07-15 18:47:17 +0430

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+apt -y update
+apt -y upgrade
+apt -y install network-manager emacs firefox-esr pcmanfm xorg xinit vim git build-essential terminator xmonad libghc-xmonad-contrib-dev suckless-tools xterm scrot neofetch picom nitrogen ttf-bitstream-vera mit-scheme clang cmake libclang-dev libtool-bin pulseaudio pavucontrol openconnect unzip unrar
+apt -y autopurge
